0d20b1c4b92bc4bfbf34b33a9b3a8271c1917341
Jacob Okamoto <github@oko.io>
2012-03-29 13:40:38 -0500

    Removed file install for tools

--- setup.py
+++ setup.py
@@ -6,37 +6,6 @@ install_dir="/usr/bin"
 
 import os,sys
 
-def parse_args():
-    # pull args from sys.argv
-    args = sys.argv
-    
-    if len(args) > 1:
-        args = args[1:]
-    else: return {}
-    
-    ret = {}
-    
-    for arg in args:
-        if arg.find("=") != -1:
-            parts = arg.split("=")
-        else:
-            parts = [arg]
-            
-        switch = parts[0]
-        if switch.startswith("--"):
-            if len(parts) > 1: ret[switch[2:]] = parts[1]
-            else: ret[switch[2:]] = True
-        else:
-            print("Invalid option: %s" % switch)
-            
-    return ret
-
-opts = parse_args()
-
-if opts.has_key("tools-prefix"):
-    install_dir=opts["tools-prefix"]
-
-
 setup(name='ProTK',
       version='0.3.0',
       description='Linguistic analysis toolkit',
@@ -44,5 +13,4 @@ setup(name='ProTK',
       author_email='okam0013@umn.edu',
       url='https://github.com/oko/protk',
       packages=['protk2','protk2.db'],
-      data_files=[(install_dir,['arff.py','ingest.py'])]
      )
